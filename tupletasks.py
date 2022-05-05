2e9eb41c4cf0c9847cc0b7dcbf4be0ef9aea80a6
Ritik Arora 1003850 <ritikarora1003850@kjlhiturs-MacBook-Pro.local>
2022-05-05 12:45:38 +0530

    created new function to swap two tuples

--- tupletasks.py
+++ tupletasks.py
@@ -16,11 +16,25 @@ def unpack_tp(tp):
     print(y)
     print(z)
 
+def swap_2tuple(tp, tp_1):
+    print("before swapping")
+    print("first tuple:", tp)
+    print("second tuple:", tp_1)
+    temp = tp
+    tp = tp_1
+    tp_1 = temp
+    print("After swapping")
+    print("First tuple:", tp)
+    print("second tuple:", tp_1)
+
+
 
 if __name__ == '__main__':
     tpl = (10, 20, 30, 40)
+    tpl1= (40, 50)
     tp1 = ("Orange", [10, 20, 30], (5, 15, 25))
     reverse_tuple(tp=tpl)
     ac_twenty(tp=tp1)
     single_item(tp=(50,))
     unpack_tp(tp=tpl)
+    swap_2tuple(tp=tpl, tp_1=tpl1)
