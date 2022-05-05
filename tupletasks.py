075ef445dec55b7caaf7197be2ba6cdbca995cf9
Ritik Arora 1003850 <ritikarora1003850@kjlhiturs-MacBook-Pro.local>
2022-05-05 12:29:52 +0530

    created new function to make tuple with obe item

--- tupletasks.py
+++ tupletasks.py
@@ -6,11 +6,13 @@ def reverse_tuple(tp):
 def ac_twenty(tp):
     print(tp[1][1])
 
-#def single_item(tp):
-    #print(tp)
+def single_item(tp):
+    print(tp)
+
 
 if __name__ == '__main__':
     tpl = (10, 20, 30, 40, 50)
     tp1 = ("Orange", [10, 20, 30], (5, 15, 25))
     reverse_tuple(tp=tpl)
     ac_twenty(tp=tp1)
+    single_item(tp=(50,))
