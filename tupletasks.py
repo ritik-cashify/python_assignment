5e290ae941418966f0da26746fa9c485c1b5209e
Ritik Arora 1003850 <ritikarora1003850@kjlhiturs-MacBook-Pro.local>
2022-05-05 12:26:55 +0530

    created new python file of tuple tasks and made two functions-reverse and one more

---
+++ tupletasks.py
@@ -0,0 +1,16 @@
+def reverse_tuple(tp):
+    rtp = reversed(tp)
+    rtp = tuple(rtp)
+    print("The reverse of the tuple is:",rtp)
+
+def ac_twenty(tp):
+    print(tp[1][1])
+
+#def single_item(tp):
+    #print(tp)
+
+if __name__ == '__main__':
+    tpl = (10, 20, 30, 40, 50)
+    tp1 = ("Orange", [10, 20, 30], (5, 15, 25))
+    reverse_tuple(tp=tpl)
+    ac_twenty(tp=tp1)
