86a1f37831bc2429b8fec006bc8d32ee971565a2
Ritik Arora 1003850 <ritikarora1003850@kjlhiturs-MacBook-Pro.local>
2022-05-05 11:10:42 +0530

    added function to print common elements from two sets

--- taskset.py
+++ taskset.py
@@ -8,5 +8,11 @@ def addlst_2set():
     set.update(list)
     print("The updated set is:",set)
 
+def common_elements():
+    print("The first set:",set)
+    print("The second set:",set1)
+    print("The common elements from both the sets are:",set.intersection(set1))
+
 if __name__=='__main__':
     addlst_2set()
+    common_elements()
