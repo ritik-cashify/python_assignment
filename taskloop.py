fb1fb384ed1b2650f0a94a30e189d4c591704e62
Ritik Arora 1003850 <ritikarora1003850@kjlhiturs-MacBook-Pro.local>
2022-05-05 15:46:10 +0530

    created new function to print a pattern

--- taskloop.py
+++ taskloop.py
@@ -4,5 +4,13 @@ def nat_nos():
         print(i)
         i += 1
 
+def pyramid():
+    rows = 4
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print(j, end=' ')
+        print('')
+
 if __name__ == '__main__':
     nat_nos()
+    pyramid()
